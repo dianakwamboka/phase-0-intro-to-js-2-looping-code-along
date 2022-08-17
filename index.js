ebeba0cd2b80edff2dfbe77204147cd649c9ac44
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-08-18 02:32:09 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(people, event) {
+    const people = [];
+    for( let i = 0; i <people.length; i++) {
+        people.push('thank you:["Guadalupe", "Ole", "Aki"] ${people[i]}, for the wonderful ${event} gift!');
+    
+    }
+    return people;
+}
+function countDown(num) {
+    while (num>=0){
+        console.log(num);
+        num --;
+    }
+}
